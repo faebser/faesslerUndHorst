c7b60b213181bfd4de3d10e69cdce436015d876e
Fabian Frei <fabian.frei@esf-frei.ch>
2011-12-16 13:21:00 +0100

    slider is working now

--- wp-content/themes/faesslerhorst/js/script.js
+++ wp-content/themes/faesslerhorst/js/script.js
@@ -1,45 +1,109 @@
 /* Author:
 	fabian.frei@openinteractive.ch
  */
-var data;
-var result;
-var slideshow;
-var slideshowItem
+
 $(document).ready(function() {
-	
+	$('#main').slideshow();
 });
 
 /**
  * Slideshow plugin
+ * @author fabian.frei@openinteractive.ch
+ * @version 1
+ * 
+ * Added hook to the plugin via functions.
+ * Use either hookBefore() or hookNextBefore()/hookPreviousBefore() and viceversa.
+ * The plugin first checks for the next/previous hooks before switching back to the general hooks.
+ * use the data object to store data which has to be accessed from everywhere in the plugin
+ * 
+ * Try using the same ids and classes => #slider for the container, .sliderItem for the items
+ * and #previous/#next for the buttons
+ * if needed you can modify these in the init method of the plugin. 
  */
 (function($) {
-	
+
 	data = {
-			amountShifting : 1
+			amountShifting : 1,
+			pointer : 0,
+			elements : {}
 	};
-	
-	 // function has to be called on the parent
-		var methods = {
-				init: function() { 
-					return this.each(function() {
-					
-					
+
+	/*function hookBefore() {
+		;
+	}
+
+	function hookAfter() {
+		;
+	}
+
+	function hookNextBefore() {
+		;
+	}*/
+
+	function hookNextAfter() { // fehler bei max amount
+		if(data.pointerIndex < data.elementsAmount-1) {
+			data.pointerIndex++;
+		}
+		else {
+			data.pointerIndex = 0;
+		}
+		console.log(data.pointer);
+		console.log(data.elementsAmount);
+		console.log(data.pointerIndex);
+		data.pointer = data.elements[data.pointerIndex];
+	}
+
+	function hookPreviousBefore() {
+		if(data.pointerIndex > 0) {
+			data.pointerIndex--;
+		}
+		else {
+			data.pointerIndex = data.elementsAmount-1;
+		}
+		console.log(data.elementsAmount);
+		console.log(data.pointer);
+		console.log(data.pointerIndex);
+		data.pointer = data.elements[data.pointerIndex];
+	}
+
+	/*function hookPreviousAfter() {
+		
+	}*/
+
+	// function has to be called on the parent
+	var methods = {
+			init: function() { 
+				return this.each(function() {
+
 					var $this = $(this);
 					var slider = $this.find("#slider");
-					var spacerSlider = $("#spacer-slider");
-					data.spacerSlider = spacerSlider;
+					/*var spacerSlider = $("#spacer-slider");
+					data.spacerSlider = spacerSlider;*/
 					data.slider = slider;
-					var back = $("#back");
+					var previous = $("#previous");
 					var next = $("#next");
-					
-					slideshowItem = $this.find(".sliderItem");
-					
-					data.slideShowItemLength = (979) * data.amountShifting;
+					data.maxWidth = 0;
+					data.elementsAmount = 0;
+					data.pointerIndex = 0;
+
+					//slideshowItem = $this.find(".sliderItem");
+
+					$('.sliderItem img').each(function(index, element) {
+						data.elements[index] = { element : $(element),  width: $(element).width() };
+						data.maxWidth += $(element).width();
+						data.elementsAmount++;
+					});
+					data.pointer = data.elements[data.pointerIndex];
+					console.log(data.elements);
+					console.log(data.pointer);
+					console.log({'width': data.maxWidth});
+
+					/*data.slideShowItemLength = (979) * data.amountShifting;
 					if(data.amountShifting == 1) {
 						data.maxWidth = Math.ceil(data.slideShowItemLength * slideshowItem.length);
 					} else {
 						data.maxWidth = Math.ceil( slideshowItem.length / data.amountShifting ) * data.slideShowItemLength;
-					}
+					}*/
 
 					/**
 					 * Added the ability to decide how many items you want to shift
@@ -47,12 +111,12 @@ $(document).ready(function() {
 					 * 
 					 * @author fabian.frei@openinteractive.ch
 					 */
-					
-					data.items = {};
+
+					/*data.items = {};
 					var url = window.location.pathname.split('/');
 					url = url[url.length-1];
 					url = url.split('.')[0];
-					
+
 					$('.sliderItem').each(function(index) {
 						var id = $(this).attr('id').split('-')[0];
 						data.items[id] = {};
@@ -64,7 +128,7 @@ $(document).ready(function() {
 							data.items[id].active = false;
 						}
 					})
-					
+
 					$('nav ul.level1 li a').click(function(event) {
 						event.preventDefault();
 						var url = $(this).attr('href').split('/');
@@ -75,41 +139,39 @@ $(document).ready(function() {
 						});
 						$(this).css('text-decoration','underline')
 						slideshow.slideshow('slideTo',url);
-					});
-					
-					// Back
-					back.click(function(event) {
-						event.preventDefault();
+					});*/
+
+					// previous
+					previous.click(function(event) {
+						event.preventDefault(); // prevent firing of default event
+
 						if (! slider.is(":animated")) {
+							// here are the hook-functions called
+							if(typeof hookPreviousBefore == 'function') {
+								hookPreviousBefore();
+							} 
+							else if(typeof hookBefore == 'function') {
+								hookBefore();
+							}
 
 							var left = parseInt(slider.css('margin-left')) * -1;
 
 							if (left == 0) {
-								left = - (data.maxWidth - data.slideShowItemLength); 
+								left = - (data.maxWidth - data.pointer.width); 
 							}
 							else {
-								left = "+=" + data.slideShowItemLength; 
-							}
-							$('nav ul.level1 li a').each( function(index) {
-								if($(this).css('text-decoration') == 'underline') {
-									$(this).css('text-decoration','none');
-									if($(this).parent().prev().length == 0) {
-										$('nav ul.level1 li a').last().css('text-decoration','underline')
-									} else {
-										$(this).parent().prev().find('a').css('text-decoration','underline');
-									}
-									
-									return false;
-								}
-							});
-							
-							
+								left = "+=" + data.pointer.width; 
+							}							
 							slider.animate({
 								marginLeft: left
 							});
-							spacerSlider.animate({
-								marginLeft: left
-							});
+							// here are the hook-functions called
+							if(typeof hookPreviousAfter == 'function') {
+								hookPreviousAfter();
+							} 
+							else if(typeof hookAfter == 'function') {
+								hookAfter();
+							}
 						}
 					});
 
@@ -117,109 +179,106 @@ $(document).ready(function() {
 					next.click(function(event) {
 						event.preventDefault();
 						if (! slider.is(":animated")) {
+							// here are the before hook-functions called
+							if(typeof hookNextBefore == 'function') {
+								hookNextBefore();
+							} 
+							else if(typeof hookNext == 'function') {
+								hookNext();
+							}
 							var left = parseInt(slider.css('margin-left')) * -1;
 
-							if (left == (data.maxWidth - data.slideShowItemLength)) {
+							if (left == (data.maxWidth - data.pointer.width)) {
 								left = 0;
 							}
 							else {
-								left = "-=" + data.slideShowItemLength;
+								left = "-=" + data.pointer.width;
 							}
-							
-							$('nav ul.level1 li a').each( function(index) {
-								if($(this).css('text-decoration') == 'underline') {
-									$(this).css('text-decoration','none');
-									console.log($(this).parent().next());
-									if($(this).parent().next().length == 0) {
-										$('nav ul.level1 li a').first().css('text-decoration','underline')
-									} else {
-										$(this).parent().next().find('a').css('text-decoration','underline');
-									}
-									
-									return false;
-								}
-							});
 
 							slider.animate({
 								marginLeft: left
 							});
-							spacerSlider.animate({
-								marginLeft: left
-							});
+							// here are the hook-functions called
+							if(typeof hookNextAfter == 'function') {
+								hookNextAfter();
+							} 
+							else if(typeof hookAfter == 'function') {
+								hookAfter();
+							}
 						}
 					});
 				});
 			}, 
-				slideTo : function(name) {
-					var prevItem, nextItem, diff;
-					for ( var item in data.items) {
-						if(data.items[item].active == true) {
-							prevItem = data.items[item];
-						}
-						if(item == name) {
-							nextItem = data.items[item];
-						}
-						if(prevItem != undefined && nextItem != undefined) {
-							diff = prevItem.position - nextItem.position;
-							break;
-						}
+			slideTo : function(name) {
+				var prevItem, nextItem, diff;
+				for ( var item in data.items) {
+					if(data.items[item].active == true) {
+						prevItem = data.items[item];
 					}
-					prevItem.active = false;
-					nextItem.active = true;
-					
-					var slider = data.slider;
-					var spacerSlider = data.spacerSlider;
-					if(diff > 0) {
-						if (! slider.is(":animated")) {
-
-							var left = parseInt(slider.css('margin-left')) * -1;
-
-							if (left == 0) {
-								left = - (data.maxWidth - data.slideShowItemLength); 
-							}
-							else {
-								left = "+=" + diff * data.slideShowItemLength;; 
-							}
-
-							slider.animate({
-								marginLeft: left
-							});
-							spacerSlider.animate({
-								marginLeft: left
-							});
-						}
-					} 
-					else if(diff < 0) {
-						if (! slider.is(":animated")) {
-							var left = parseInt(slider.css('margin-left')) * -1;
-							
-							if (left == (data.maxWidth - data.slideShowItemLength)) {
-								left = 0;
-							}
-							else {
-								left = "-=" + (diff*-1) * data.slideShowItemLength;;
-							}
-
-							slider.animate({
-								marginLeft: left
-							});
-							spacerSlider.animate({
-								marginLeft: left
-							});
-						}
+					if(item == name) {
+						nextItem = data.items[item];
 					}
+					if(prevItem != undefined && nextItem != undefined) {
+						diff = prevItem.position - nextItem.position;
+						break;
+					}
+				}
+				prevItem.active = false;
+				nextItem.active = true;
+
+				var slider = data.slider;
+				var spacerSlider = data.spacerSlider;
+				if(diff > 0) {
+					if (! slider.is(":animated")) {
+
+						var left = parseInt(slider.css('margin-left')) * -1;
+
+						if (left == 0) {
+							left = - (data.maxWidth - data.slideShowItemLength); 
+						}
+						else {
+							left = "+=" + diff * data.slideShowItemLength;; 
+						}
+
+						slider.animate({
+							marginLeft: left
+						});
+						spacerSlider.animate({
+							marginLeft: left
+						});
+					}
+				} 
+				else if(diff < 0) {
+					if (! slider.is(":animated")) {
+						var left = parseInt(slider.css('margin-left')) * -1;
+
+						if (left == (data.maxWidth - data.slideShowItemLength)) {
+							left = 0;
+						}
+						else {
+							left = "-=" + (diff*-1) * data.slideShowItemLength;;
+						}
+
+						slider.animate({
+							marginLeft: left
+						});
+						spacerSlider.animate({
+							marginLeft: left
+						});
+					}
+				}
 			}
+	}
+
+	$.fn.slideshow = function(method) {
+		// Method calling logic
+		if ( methods[method] ) {
+			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+			return methods.init.apply( this, arguments );
+		} else {
+			$.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
 		}
-		
-		$.fn.slideshow = function(method) {
-			 // Method calling logic
-			if ( methods[method] ) {
-				return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-			} else if ( typeof method === 'object' || ! method ) {
-				return methods.init.apply( this, arguments );
-			} else {
-				$.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
-			}
-		};
+	};
 })(jQuery);
 
