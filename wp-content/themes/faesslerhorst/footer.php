d2af108f3f8670d673a71d71fda8ad58e846fc08
Fabian Frei <fabian.frei@esf-frei.ch>
2012-01-31 13:16:45 +0100

    first running template

--- wp-content/themes/faesslerhorst/footer.php
+++ wp-content/themes/faesslerhorst/footer.php
@@ -7,5 +7,25 @@
 * @subpackage faesslerUndHorstFotoKanzlei
 * @since faesslerUndHorstFotoKanzlei 0.001
 */
-
 ?>
+<footer>
+
+</footer>
+</div>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+<script>window.jQuery||document.write('<script src="<?php echo get_template_directory_uri(); ?>js/libs/jquery-1.6.2.min.js"><\/script>');</script>
+
+
+<script defer src='<?php echo get_template_directory_uri(); ?>/js/plugins.js'></script>
+<script defer src='<?php echo get_template_directory_uri(); ?>/js/script.js'></script>
+
+
+<!--[if lt IE 7 ]><script defer src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+<script defer>window.attachEvent("onload",function(){
+	CFInstall.check({
+		mode:"overlay"})});</script><![endif]-->
+		<?php wp_footer(); ?>
+
+		</body>
+		</html>
--- wp-content/themes/faesslerhorst/header.php
+++ wp-content/themes/faesslerhorst/header.php
@@ -8,5 +8,23 @@
 * @subpackage faesslerUndHorstFotoKanzlei
 * @since faesslerUndHorstFotoKanzlei 0.001
 */
-
 ?>
+<!doctype html>
+
+<!--[if lt IE 7]><html class="no-js ie6 oldie" lang=de><![endif]-->
+<!--[if IE 7]><html class="no-js ie7 oldie" lang=de><![endif]-->
+<!--[if IE 8]><html class="no-js ie8 oldie" lang=de><![endif]-->
+
+<!--[if gt IE 8]><!--> <html class=no-js lang=de> <!--<![endif]-->
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" >
+
+<title><?php bloginfo( 'name' ); ?></title>
+<meta name=description content="">
+<meta name=author content="">
+<link type="text/css" rel=stylesheet href="http://fonts.googleapis.com/css?family=Open+Sans:300,400">
+<meta name=viewport content="width=device-width,initial-scale=1">
+<link rel=stylesheet href='<?php echo get_template_directory_uri(); ?>/css/style.css'>
+<script src="<?php echo get_template_directory_uri(); ?>/js/libs/modernizr-2.0.6.min.js"></script>
+<?php wp_head(); ?>
+</head>
